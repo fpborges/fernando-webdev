6bc76bbf5f18f3c0b60e1310f41f6600b50aad1d
Fernando <fernandop@pmgintelligence.com>
2021-08-10 14:49:56 -0400

    chartjs updated

--- src/charts/skillsData.js
+++ src/charts/skillsData.js
@@ -1,4 +1,138 @@
-export const planetChartData = {
+import Chart from "chart.js"; //import Chart for global properties
+
+//Set global animation duration
+Chart.defaults.global.animation.duration = 3000;
+Chart.defaults.global.legend.display = false;
+
+var barDataSet = [90, 80, 85, 65, 75, 40, 70, 95, 70, 60];
+
+function randomColors() {
+	let barColors = [];
+
+	let dynamicColors = () => {
+		var r = Math.floor(Math.random() * 255);
+		var g = Math.floor(Math.random() * 255);
+		var b = Math.floor(Math.random() * 255);
+		return "rgba(" + r + "," + g + "," + b + ", 0.3)";
+	};
+
+	barDataSet.forEach(() => {
+		barColors.push(dynamicColors());
+	});
+
+	return barColors;
+}
+
+export const frontEnd = {
+	type: "horizontalBar",
+	data: {
+		labels: [
+			"HTML5/CSS3",
+			"Javascript",
+			"Vue.js",
+			"Jquery",
+			"WordPress",
+			"Restful APIs",
+			"UI/UX",
+			"UI library/framework",
+			"Git",
+			"Testing",
+		],
+		datasets: [
+			{
+				label: "",
+				data: barDataSet,
+				backgroundColor: randomColors(),
+				borderColor: randomColors(),
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		animation: {
+			duration: 3000,
+		},
+		tooltips: {
+			callbacks: {
+				label: function (tooltipItem, data) {
+					var value = data.datasets[0].data[tooltipItem.index];
+					return "experience: " + value + "%";
+				},
+			},
+		},
+		scales: {
+			xAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+						max: 100,
+						stepSize: 10,
+						callback: (value) => {
+							return value + "%";
+						},
+					},
+					gridLines: {
+						display: false,
+					},
+				},
+			],
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+
+					gridLines: {
+						display: false,
+					},
+				},
+			],
+		},
+	},
+};
+
+export const backEnd = {
+	type: "horizontalBar",
+	data: {
+		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+				},
+			],
+		},
+	},
+};
+
+export const desingSkills = {
 	type: "line",
 	data: {
 		labels: [
@@ -43,25 +177,107 @@ export const planetChartData = {
 		},
 	},
 };
-
+var delayed;
 export const myChart = {
 	type: "horizontalBar",
 	data: {
-		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		labels: ["Javascript"],
 		datasets: [
 			{
-				axis: "y",
-				label: "My First Dataset",
-				data: [65, 59, 80, 81, 56, 55, 40],
-				fill: false,
+				label: "Users Sources",
+				data: [56],
+				// backgroundColor: "#2f3c5a",
+				// borderColor: "#2f3c5a",
+				// borderWidth: 1,
+				// hoverBorder: 1,
+				// hoverBorderColor: "rgba(180, 181, 183)",
+				// categoryPercentage: 1.0,
+				backgroundColor: "#2f3c5a",
+				borderColor: "#2f3c5a",
+				borderWidth: 1,
+				hoverBorder: 1,
+				hoverBorderColor: "rgba(180, 181, 183)",
+				barThickness: 50,
+			},
+		],
+	},
+	options: {
+		tooltips: {
+			mode: "index",
+			axis: "y",
+		},
+		animation: {
+			onComplete: () => {
+				delayed = true;
+			},
+
+			delay: (context) => {
+				let delay = 0;
+				if (context.type === "data" && context.mode === "default" && !delayed) {
+					delay = context.dataIndex * 300 + context.datasetIndex * 100;
+				}
+				return delay;
+			},
+		},
+
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: false,
+						max: 100,
+					},
+					gridLines: {
+						display: false,
+					},
+				},
+			],
+			xAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+						max: 100,
+						stepSize: 10,
+						callback: function (value) {
+							return value + "%";
+						},
+					},
+					scaleLabel: {
+						display: true,
+						labelString: "Knowledge Level",
+					},
+					gridLines: {
+						display: false,
+					},
+				},
+			],
+		},
+	},
+};
+
+export const mySkills = {
+	type: "horizontalBar",
+	data: {
+		labels: ["HTML5"],
+		datasets: [
+			{
+				label: "asdasd",
+				data: [34],
+				borderWidth: 1,
+				hoverBackgroundColor: "rgba(232,105,90,0.8)",
+				hoverBorderColor: "orange",
+				barThickness: 50,
+
 				backgroundColor: [
-					"rgba(255, 99, 132, 0.2)",
-					"rgba(255, 159, 64, 0.2)",
-					"rgba(255, 205, 86, 0.2)",
-					"rgba(75, 192, 192, 0.2)",
-					"rgba(54, 162, 235, 0.2)",
-					"rgba(153, 102, 255, 0.2)",
-					"rgba(201, 203, 207, 0.2)",
+					"rgba(64, 71, 86, 0.8)",
+					"rgba(149, 80, 140, 0.8)",
+					"rgba(149, 245, 140, 0.8)",
+					"rgba(101, 63, 0, 0.8)",
+					"rgba(72, 151, 246, 0.8)",
+					"rgba(0, 138, 96, 0.8)",
+					"rgba(124, 145, 188, 0.8)",
+					"rgba(229, 119, 55, 0.8)",
+					"rgba(64, 71, 86, 0.8)",
 				],
 				borderColor: [
 					"rgb(255, 99, 132)",
@@ -72,26 +288,57 @@ export const myChart = {
 					"rgb(153, 102, 255)",
 					"rgb(201, 203, 207)",
 				],
-				borderWidth: 1,
+
+				hoverBorder: 2,
 			},
 		],
-
-		options: {
-			indexAxis: "y",
-			scales: {
-				xAxes: [
-					{
-						stacked: true,
-					},
-				],
-				yAxes: [
-					{
-						stacked: true,
-					},
-				],
+	},
+	options: {
+		legend: {
+			labels: {
+				fontColor: "blue",
+				color: "red",
+				fontSize: 18,
 			},
+		},
+		scales: {
+			yAxes: [
+				{
+					gridLines: {
+						display: false,
+						drawBorder: false,
+					},
+				},
+			],
+			xAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+						max: 100,
+						stepSize: 10,
+						callback: function (value) {
+							return value + "%";
+						},
+					},
+					gridLines: {
+						display: false,
+					},
+				},
+			],
 		},
 	},
 };
 
-export default { planetChartData, myChart };
+function randCol() {
+	frontEnd.data.datasets[0].backgroundColor = randomColors();
+
+	frontEnd.update();
+
+	console.log(frontEnd.data.datasets[0].backgroundColor);
+	// frontEnd.data.datasets.forEach((dataset) => {
+	// 	dataset.data.pop();
+	// });
+	// frontEnd.update();
+}
+
+export default { myChart, mySkills, frontEnd, randCol };
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,31 +2,90 @@
 	<div class="Skills">
 		<v-container>
 			<h1>My Developed Skills</h1>
-			*Examples*
-			<canvas id="planet-chart"></canvas>
-			<canvas id="my-chart"></canvas>
+			<v-row>
+				<v-col>
+					<v-card elevation="2">
+						<v-card-title>Frontend Skills</v-card-title>
+						<v-card-text>
+							<canvas class="frontend"></canvas>
+						</v-card-text>
+						<v-card-actions>
+							<v-btn @click="randColors()">Randomize Colors</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-col>
+				<v-col>
+					<v-card elevation="2">
+						<v-card-title>Backend Skills</v-card-title>
+						<v-card-text>
+							<canvas class="backend"></canvas>
+						</v-card-text>
+					</v-card>
+				</v-col>
+			</v-row>
+			<v-row>
+				<v-col>
+					<v-card elevation="2">
+						<v-card-title>Design Skills</v-card-title>
+						<v-card-text>
+							<!-- photoshop
+							justinmind
+							elementor
+							chartjs
+							d3.js
+							MockFlow
+							camtasia Video Editor -->
+							<canvas class="design"></canvas>
+						</v-card-text>
+					</v-card>
+				</v-col>
+			</v-row>
+			<canvas id="my-skills"></canvas>
+			<canvas id="myChart" width="400" height="400"></canvas>
 		</v-container>
 	</div>
 </template>
 
 <script>
 	import { skillsMixinsData } from "../mixins";
-	import { planetChartData, myChart } from "../charts/skillsData.js";
+	import {
+		mySkills,
+		myChart,
+		frontEnd,
+		backEnd,
+		desingSkills,
+	} from "../charts/skillsData.js";
+	import myFunctions from "@/charts/skillsData";
+
 	import Chart from "chart.js";
 
 	export default {
 		name: "Skills",
 		mixins: [skillsMixinsData],
 		data: () => ({
-			test: "test",
-			planetChartData,
+			mySkills,
 			myChart,
+			frontEnd,
+			backEnd,
+			desingSkills,
 		}),
 		mounted() {
-			const ctx = document.querySelector("#planet-chart");
-			const mch = document.querySelector("#my-chart");
-			new Chart(ctx, this.planetChartData);
-			new Chart(mch, this.myChart);
+			const ctx = document.querySelector("#my-skills");
+			const ctx2 = document.querySelector("#myChart");
+			const frontSelector = document.querySelector(".frontend");
+			const backSelector = document.querySelector(".backend");
+			const designSelector = document.querySelector(".design");
+
+			new Chart(ctx, this.mySkills);
+			new Chart(ctx2, this.myChart);
+			new Chart(frontSelector, this.frontEnd);
+			new Chart(backSelector, this.backEnd);
+			new Chart(designSelector, this.desingSkills);
+		},
+		methods: {
+			randColors() {
+				myFunctions.randCol();
+			},
 		},
 	};
 </script>
