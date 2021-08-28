5411090e5b5005f662dcdbb13d96379bdadc1a6b
Fernando <fernandop@pmgintelligence.com>
2021-08-27 22:54:23 -0400

    projects updated

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,15 +1,16 @@
 <template>
 	<div class="Projects">
 		<v-container>
-			<v-row>
-				<v-col class="text-center">
+			<v-row justify="center" no-gutters>
+				<v-col md="auto">
 					<h2>Live Projects</h2>
 					<div class="dot"></div>
+					<!-- <v-card class="pa-2"> Live Projects </v-card><span class="dot"></span> -->
 				</v-col>
 			</v-row>
 			<v-row>
 				<v-col>
-					<h4>QBORD</h4>
+					<h3>Web Applications</h3>
 					<div class="desktop">
 						<div class="content">
 							<a href="https://www.qbord.com/" target="blank">
@@ -19,6 +20,24 @@
 							/></a>
 						</div>
 					</div>
+				</v-col>
+			</v-row>
+			<v-row class="mt-10">
+				<h3>GitHub</h3>
+				<v-col>
+					<p>Word Scrumble</p>
+					<a href="https://github.com/fpborges/Word_Scramble_WCF_Project.git">
+						<img
+							src="https://word.tips/pictures/d45ff610-e6f5-4f4c-a04e-1e1bc3c3bd1a_scramble_words.jpg?auto=compress,format"
+					/></a>
+				</v-col>
+				<v-col>
+					<p>Exp Trip mobile app</p>
+					<a href="https://github.com/fpborges/ExpTrip.git">
+						<img
+							src="https://github.com/fpborges/ExpTrip/blob/master/images/img_welcome.jpg?raw=true"
+							height="400"
+					/></a>
 				</v-col>
 			</v-row>
 		</v-container>
@@ -45,6 +64,8 @@
 		border: 1px solid grey;
 		background-color: chartreuse;
 		float: left;
+		margin-top: 5px;
+		margin-left: 5px;
 	}
 
 	.previewOnScreen {
